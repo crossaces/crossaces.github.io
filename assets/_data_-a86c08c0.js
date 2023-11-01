e85aa5726b48cc96ed7c43a5b6249ea95ec151db
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2023-11-01 07:24:37 +0700

    update

--- assets/_data_-a86c08c0.js
+++
@@ -1,1 +0,0 @@
-import{u as t,j as s,L as o}from"./index-e5f4eac7.js";import"./react-43ab3c9e.js";import"./react-dom-c58a95f5.js";const m=()=>{const{data:a}=t();return s.jsx(o,{children:s.jsxs("div",{className:"container-layout",children:["my blog ",a]})})};export{m as default};
--- assets/index-fa8aa590.js
+++
@@ -1,1 +0,0 @@
-import{j as t,L as o}from"./index-e5f4eac7.js";import"./react-43ab3c9e.js";import"./react-dom-c58a95f5.js";const i=()=>t.jsx(o,{children:t.jsx("div",{className:"container-layout",children:"my blog without params"})});export{i as default};
