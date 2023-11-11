004f318a02e9563bd3c7456a5f25398b28825a8f
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2023-11-11 23:18:16 +0700

    update

--- assets/_data_-e8d25438.js
+++
@@ -1,1 +0,0 @@
-import{u as s,j as o,a as t,L as r}from"./index-4067034f.js";const l=()=>{const{data:a}=s();return o(r,{children:t("div",{className:"container-layout",children:["my blog ",a]})})};export{l as default};
--- assets/index-ef879d32.js
+++
@@ -1,1 +0,0 @@
-import{j as a,L as o}from"./index-4067034f.js";const r=()=>a(o,{children:a("div",{className:"container-layout",children:"my blog without params"})});export{r as default};
